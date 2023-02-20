7118237a3b513fdb7759fadab96523859a23ef3f
Cong <phamconggtc187@gmail.com>
2023-02-20 21:27:47 +0700

    Buoi 12 Demo for loop

---
+++ Buổi 12 15-2-2023/demo_for_loop.js
@@ -0,0 +1,17 @@
+// in ra màn hình các số từ 1 đến 10
+// for (let i = 1; i < 11; i++) {
+//     document.write(i + '<br>');
+// }
+
+// in ra màn ình các số từ 10 đến 1
+// for (let i = 10; i > 0; i--) {
+//       document.write(i + '<br>');
+// }
+
+// for (let i = 1.0; i < 2; i += 0.1) {
+//     document.write(i + '<br>');
+// }
+
+for (let i = 0; i<=100; i += 2) {
+        document.write(i + '<br>');
+     }
